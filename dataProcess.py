16e5972388a1a92af6f59e00f102a1e4b2d80905
panhanlin16 <1091693650@qq.com>
2021-10-24 10:45:53 +0800

    添加数据处理

---
+++ dataProcess.py
@@ -0,0 +1,6 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+excel_path="D:/111hk/py/pj/"
+excel_name="train data credit card.csv"
+data = pd.read_csv(excel_path+excel_name)
